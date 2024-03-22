7277571e19747b35084046b0aa9e2e4849fdab97
KushVrin <163734158+KushVrin@users.noreply.github.com>
2024-03-22 14:57:35 +0530

    Update app-routing.module.ts

--- rodins-ui/src/app/app-routing.module.ts
+++ rodins-ui/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 #hello world
+#How u doin
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
